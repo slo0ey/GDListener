1ac85d3a1b3b5b5c0e91b59c65b51a62cc8ac0c9
mir99712 <mir99712@naver.com>
2020-07-12 22:10:46 +0900

    Added notification channels

--- app/src/main/java/com/github/DenFade/gdlistener/MainActivity.java
+++ app/src/main/java/com/github/DenFade/gdlistener/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,10 +16,14 @@ import android.widget.Toast;
 
 import com.github.DenFade.gdlistener.utils.FileStream;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
@@ -147,14 +152,61 @@ public class MainActivity extends AppCompatActivity {
         );
         channelForService.setDescription(getString(R.string.service_channel_desc));
         notificationManager.createNotificationChannel(channelForService);
-
-        NotificationChannel channelForEvents = new NotificationChannel(
+        List<NotificationChannelInfo> channelInfos = new ArrayList<>();
+        channelInfos.add(new NotificationChannelInfo(
                 getString(R.string.awarded_event_channel_id),
-                getString(R.string.awarded_event_channel_name),
-                NotificationManager.IMPORTANCE_DEFAULT
-        );
-        notificationManager.createNotificationChannel(channelForEvents);
+                getString(R.string.awarded_event_channel_name)
+        ));
+        channelInfos.add(new NotificationChannelInfo(
+                getString(R.string.followed_event_channel_id),
+                getString(R.string.followed_event_channel_name)
+        ));
+        channelInfos.add(new NotificationChannelInfo(
+                getString(R.string.timely_event_channel_id),
+                getString(R.string.timely_event_channel_name)
+        ));
+        for(NotificationChannelInfo info : channelInfos) {
+            Log.d("NCI", info.toString());
+            NotificationChannel channel = new NotificationChannel(
+                    info.getId(),
+                    info.getName(),
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            String desc = info.getDescription();
+            if(desc!=null){
+                channel.setDescription(desc);
+            }
+            notificationManager.createNotificationChannel(channel);
+        }
 
 
+    }
+    private class NotificationChannelInfo {
+        private String id;
+        private String name;
+        private String description = null;
+        NotificationChannelInfo(String id, String name, String description){
+            this.id = id;
+            this.name = name;
+            this.description = description;
+        }
+        NotificationChannelInfo(String id, String name){
+            this.id = id;
+            this.name = name;
+        }
+        public String getId(){
+            return id;
+        }
+        public String getName(){
+            return name;
+        }
+        public String getDescription(){
+            return description;
+        }
+        @NotNull
+        @Override
+        public String toString(){
+            return "{id="+this.id+", name="+this.name+", description="+this.description+"}";
+        }
     }
 }
